2a80b68d122d57db89e89aaef7595fe9bf9841d4
sixthson6 <sudais2408@gmail.com>
2024-10-10 12:46:16 +0000

    Alx interview

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Traverse lockboxes
+"""
+
+def canUnlockAll(boxes):
+    """Check if all locked boxes can
+    be unlocked"""
+    unlocked = set([0])
+    keys = set()
+
+    for i in range(len(boxes)):
+        if i in unlocked:
+            keys.update(boxes[i])
+
+            for key in keys:
+                if key not in unlocked and key < len(boxes):
+                    unlocked.add(key)
+    return len(unlocked) == len(boxes)
